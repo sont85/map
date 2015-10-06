851b9cdea66ba2d162eca02b025bd4b7a0538a2e
Son Truong <sont85@gmail.com>
2015-10-05 21:08:42 -0700

    empty local storage edge case

--- src/app.jsx
+++ src/app.jsx
@@ -2,44 +2,34 @@
   var App = React.createClass({
     getInitialState: function() {
       var favorites = JSON.parse(localStorage.getItem('coordinates'));
-      return favorites
-        ? {
+      if (favorites) {
+        return {
           favorites : favorites,
           currentAddress : favorites[0].address,
           lat : favorites[0].H,
           lng : favorites[0].L
-        }
-        : null;
+        };
+      } else {
+        return {  favorites : []};
+      }
     },
     searchForAddress: function(address) {
-      // GMaps.geocode({
-      //   address: address,
-      //   callback: (function(results, status) {
-      //     console.log(results);
-      //     if (status != 'OK')
-      //       return;
-      //     var latlng = results[0].geometry.location;
-      //     latlng.address = results[0].formatted_address;
-      //     this.saveLocation(latlng);
-      //     this.setState({
-      //       currentAddress: latlng.address,
-      //       lat: latlng.lat(),
-      //       lng: latlng.lng()
-      //     });
-      //   }).bind(this)
-      // });
-
-      new google.maps.Geocoder().geocode({ address: address }, (function(results, status) {
-        if (status != 'OK') return;
-        var latlng = results[0].geometry.location;
-        latlng.address = results[0].formatted_address;
-        this.saveLocation(latlng);
-        this.setState({
-          currentAddress: latlng.address,
-          lat: latlng.lat(),
-          lng: latlng.lng()
-        });
-      }).bind(this));
+      new google.maps
+        .Geocoder()
+        .geocode({
+          address: address
+        }, (function(results, status) {
+          if (status != 'OK')
+            return;
+          var latlng = results[0].geometry.location;
+          latlng.address = results[0].formatted_address;
+          this.saveLocation(latlng);
+          this.setState({
+            currentAddress: latlng.address,
+            lat: latlng.lat(),
+            lng: latlng.lng()
+          });
+        }).bind(this));
     },
     saveLocation: function(latlng) {
       var storage = localStorage.getItem('coordinates');
